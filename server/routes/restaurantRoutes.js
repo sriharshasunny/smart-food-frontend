a13bd95e83071c42582884341261554b7525db69
Boindla Sriharsha <sriharshabboindla@gmail.com>
2026-03-10 23:15:05 +0530

    fix: add missing path import in restaurantRoutes.js

--- server/routes/restaurantRoutes.js
+++ server/routes/restaurantRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const supabase = require('../utils/supabase');
 const restaurantOrderController = require('../controllers/restaurantOrderController');
 
