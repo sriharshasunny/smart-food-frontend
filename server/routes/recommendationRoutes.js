d7662baa3b375027b6624839fb754948c5d2de7b
Boindla Sriharsha <sriharshabboindla@gmail.com>
2026-03-21 23:12:53 +0530

    fix(backend): correct recommendationRoutes to use functional controllers instead of broken draft directory

--- server/routes/recommendationRoutes.js
+++ server/routes/recommendationRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const expressRouter = express.Router();
-const recommendationController = require('../recommendation/recommendationController');
+const recommendationController = require('../controllers/recommendationController');
 
 // Native Recommendation REST APIs
 expressRouter.get('/:userId', recommendationController.getPersonalizedRecommendations);
-expressRouter.get('/location/:userId/:location', recommendationController.getLocationAwareRecommendations);
-expressRouter.get('/similar/:foodId', recommendationController.getSimilarFoods);
-expressRouter.get('/trending/global', recommendationController.getTrending);
-expressRouter.get('/explore/:userId', recommendationController.getExploreSelection);
+expressRouter.get('/location/:userId/:location', recommendationController.getLocationBasedRecommendations);
+expressRouter.get('/similar/:foodId', recommendationController.getSimilarFoodRecommendations);
+expressRouter.get('/ufo-message/:userId', recommendationController.getUFOMessage);
 
 // Activity Tracking API
 expressRouter.post('/track', recommendationController.trackUserActivity);
